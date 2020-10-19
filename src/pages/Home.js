d704e450587bd76e9016cb2431e258f00566ddf6
Alex Baker <baker.alex.j@gmail.com>
2020-10-19 17:42:13 -0600

    Scrollbar removal, update to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,17 @@ import profileImg from '../images/profile.png';
 
 const Home = () => {
   return (
-    <PageOverview profileImgSrc={profileImg} header="Alex Baker">Welcome to my personal Portfolio!</PageOverview>
+    <PageOverview profileImgSrc={profileImg} header="Alex Baker">
+      <div>
+      Welcome to my personal Portfolio! <br />
+      <br />
+      I am an aspiring web engineer from Provo, UT with a primary interest in data collection and analysis. I also have an eye for 
+      user experience (application layout), and am hoping to develope this skill more over time. <br />
+      <br />
+      In fact, this portfolio was written mostly from scratch using ReactJS! Feel free to take a look at the sourcecode in the link below!
+      <br />
+      </div>
+    </PageOverview>
   )
 };
 
