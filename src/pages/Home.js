ec0035b31b712f446e3fe3f9458471f6ab93002c
Alex Baker <baker.alex.j@gmail.com>
2020-10-19 17:48:18 -0600

    Github link and logo added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { PageOverview } from '../layout';
 import profileImg from '../images/profile.png';
+import github_logo from '../images/GitHub-Mark-32px.png';
 
 const Home = () => {
   return (
@@ -9,11 +10,13 @@ const Home = () => {
       <div>
       Welcome to my personal Portfolio! <br />
       <br />
-      I am an aspiring web engineer from Provo, UT with a primary interest in data collection and analysis. I also have an eye for 
-      user experience (application layout), and am hoping to develope this skill more over time. <br />
+      I am an aspiring web engineer from Provo, UT with a primary interest in data collection and analysis. I also 
+      have an eye for user experience (application layout), and am hoping to develope this skill more over time. <br />
       <br />
-      In fact, this portfolio was written mostly from scratch using ReactJS! Feel free to take a look at the sourcecode in the link below!
-      <br />
+      In fact, this portfolio was written mostly from scratch using ReactJS! Feel free to take a look at the sourcecode in 
+      the link below! <br />
+      <br /> 
+      <img src={github_logo}/><a href="https://github.com/AlexJetB/alexjoebaker-portfolio.git">github.com/AlexJetB/alexjoebaker-portfolio.git</a>
       </div>
     </PageOverview>
   )
